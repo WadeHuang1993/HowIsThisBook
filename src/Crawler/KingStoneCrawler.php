e91de117114dbf97d3a0ca27dbdf26d11b87e878
wade <wade@doubleservice.com>
2018-07-25 11:57:18 +0800

    feat: 爬蟲：新增「搜尋書籍並擷取書籍資料」功能
    調整項目：
    1.  用書本名稱搜尋：發送爬蟲至書商網站搜尋書本
    2. 擷取搜尋結果列表的書籍
    3. 回傳擷取結果 => 多本書籍資料的陣列
    
    issue #爬蟲：新增「搜尋書籍並擷取書籍資料」功能

--- src/Crawler/KingStoneCrawler.php
+++ src/Crawler/KingStoneCrawler.php
@@ -3,6 +3,7 @@
 namespace Src\Crawler;
 
 use Goutte\Client;
+use Sunra\PhpSimple\HtmlDomParser;
 
 class KingStoneCrawler
 {
@@ -10,6 +11,7 @@ class KingStoneCrawler
     protected $response;
     protected $url;
     protected $html;
+    protected $baseUrl = 'https://www.kingstone.com.tw';
 
     /**
      * KingStoneCrawler 建構方法.
@@ -24,14 +26,15 @@ class KingStoneCrawler
     /**
      * 發送爬蟲擷取頁面
      *
-     * @param string $url URL
-     * @param string $method HTTP Methods
+     * @param string $url        URL
+     * @param string $method     HTTP Methods
+     * @param array  $parameters HTTP 參數（GET 無法使用）
      *
      * @return $this
      */
-    public function send($url, $method = 'GET')
+    public function send(string $url, string $method = 'GET', array $parameters = [])
     {
-        $this->response = $this->client->request($method, $url);
+        $this->response = $this->client->request($method, $url, $parameters);
 
         return $this;
     }
@@ -51,10 +54,18 @@ class KingStoneCrawler
     /**
      * 擷取書籍資料
      *
+     * 金石堂每本書籍都有一個 kmcode，
+     * 進入書籍內容頁面也是依照 kmcode 來取得書籍資料。
+     *
+     * @param string $kmcode 金石堂書籍唯一碼
+     *
      * @return array
      */
-    public function getBookInfo()
+    public function getBookInfo(string $kmcode): array
     {
+        $url = $this->baseUrl . '/basics/basics.asp?kmcode=' . $kmcode;
+        $this->send($url)->html();
+
         return [
             'ISBN'        => $this->getISBN(),
             'name'        => $this->getBookName(),
@@ -67,11 +78,82 @@ class KingStoneCrawler
     }
 
     /**
+     * 搜尋書籍
+     *
+     * 利用書籍名稱，至書籍網站搜尋書籍，
+     * 若搜尋結果為多筆，則擷取多筆書籍資料。
+     *
+     * @param string $bookName 書籍名稱
+     *
+     * @return array 書籍資料（多筆）
+     */
+    public function searchBook(string $bookName = ''): array
+    {
+        // 金石堂做了兩次 urlencode
+        $bookName = urlencode(urlencode($bookName));
+
+        $url = $this->makeSearchUrl($bookName);
+        $this->send($url);
+
+        // 擷取書籍 kmcode
+        $kmcodes = $this->extractSearchResult();
+
+        // 擷取書籍內容
+        $booksInfo = [];
+        foreach ($kmcodes as $kmcode) {
+            $booksInfo[] = $this->getBookInfo($kmcode);
+        }
+
+        return $booksInfo;
+    }
+
+    /**
+     * 擷取搜尋結果
+     *
+     * 在搜尋結果列表中，先擷取書籍的連結（多個），再從連結擷取書籍的 kmcode，供擷取書籍內容用。
+     *
+     * @return array $kmcodes 書籍的 kmcode（多個）
+     */
+    protected function extractSearchResult(): array
+    {
+        $parser     = HtmlDomParser::str_get_html($this->html());
+        $resultLink = $parser->find('.row_list > ul > li .anchor');
+
+        $kmcodes = $this->extractKmcode($resultLink);
+
+        return $kmcodes;
+    }
+
+    /**
+     * 從搜尋結果列表中的連結擷取 金石堂書籍唯一碼
+     *
+     * @param array $resultsList 搜尋結果列表
+     *
+     * @return array
+     */
+    protected function extractKmcode(array $resultsLinks): array
+    {
+        $results = [];
+
+        foreach ($resultsLinks as $link) {
+            $href = html_entity_decode($link->attr['href']);
+
+            // 金石堂的書籍唯一碼，可用來進入書本內容頁面
+            $kmcode = $this->htmlMatch('/kmcode=(.*)&lid/', $href);
+
+            // 過濾重複的 kmcode
+            $results[$kmcode] = $kmcode;
+        }
+
+        return $results;
+    }
+
+    /**
      * 擷取「ISBN 國際標準書號」
      *
-     * @return mixed
+     * @return string
      */
-    public function getISBN()
+    public function getISBN(): string
     {
         return $this->htmlMatch('/<span>ISBN：<\/span><em>(\d+)<\/em>/');
     }
@@ -79,9 +161,9 @@ class KingStoneCrawler
     /**
      * 擷取「書名」
      *
-     * @return mixed
+     * @return string
      */
-    public function getBookName()
+    public function getBookName(): string
     {
         return $this->htmlMatch('/<h1>(.*)\r\n/');
     }
@@ -89,9 +171,9 @@ class KingStoneCrawler
     /**
      * 擷取「作者」
      *
-     * @return mixed
+     * @return string
      */
-    public function getAuthor()
+    public function getAuthor(): string
     {
         return $this->htmlMatch('/<span>作者：<em><a.*;">(.*)<\/a>/');
     }
@@ -99,9 +181,9 @@ class KingStoneCrawler
     /**
      * 擷取「出版社」
      *
-     * @return mixed
+     * @return string
      */
-    public function getPublishing()
+    public function getPublishing(): string
     {
         return $this->htmlMatch('/<span>出版社：<em><a.*;">(.*)<\/a>/');
     }
@@ -109,9 +191,9 @@ class KingStoneCrawler
     /**
      * 擷取「譯者」
      *
-     * @return mixed
+     * @return string
      */
-    public function getTranslator()
+    public function getTranslator(): string
     {
         return $this->htmlMatch('/<span>譯者：<\/span><em>(.*)<\/em>/');
     }
@@ -119,9 +201,9 @@ class KingStoneCrawler
     /**
      * 擷取「副標題」
      *
-     * @return mixed
+     * @return string
      */
-    public function getSubTitle()
+    public function getSubTitle(): string
     {
         return $this->htmlMatch('/<em><a href="#this".*;">(.*)<\/a><\/em>/');
     }
@@ -129,9 +211,9 @@ class KingStoneCrawler
     /**
      * 擷取「出版日期」
      *
-     * @return mixed
+     * @return string
      */
-    public function getPublishDate()
+    public function getPublishDate(): string
     {
         return $this->htmlMatch('/<span>出版日：<\/span><em>(.*)<\/em>/');
     }
@@ -139,15 +221,32 @@ class KingStoneCrawler
     /**
      * 擷取 HTML 頁面資訊
      *
-     * @param String $pattern 正規表示方式規則
+     * @param string $pattern 正規表示方式規則
+     * @param string $html 被查詢對象
      *
-     * @return mixed|string
+     * @return string
      */
-    protected function htmlMatch($pattern)
+    protected function htmlMatch(string $pattern, string $html = ''): string
     {
+        $html = (trim($html) != '') ? $html : $this->html();
+
         $match = [];
-        preg_match($pattern, $this->html, $match);
+        preg_match($pattern, $html, $match);
 
         return isset($match[1]) ? $match[1] : '';
+    }
+
+    /**
+     * @param $bookName
+     *
+     * @return string
+     */
+    protected function makeSearchUrl(string $bookName): string
+    {
+        $url = $this->baseUrl . '/search/result.asp';
+        $url .= '?c_name=' . $bookName;
+        $url .= '&se_type=4';
+
+        return $url;
     }
 }
--- tests/unit/Crawler/testKingStoneCrawler.php
+++ tests/unit/Crawler/testKingStoneCrawler.php
@@ -12,14 +12,13 @@ class testKingStoneCrawler extends PHPUnit\Framework\TestCase
     public function testGetBookInfo()
     {
         // Arrange
-        $url     = 'https://www.kingstone.com.tw/basics/basics.asp?kmcode=2013120471221&actid=ActBillBoard';
+        $kmcode = '2013120471221';
 
         $client  = new Client();
         $crawler = new KingStoneCrawler($client);
-        $crawler->send($url)->html();
 
         // Act
-        $crawler->getBookInfo();
+        $bookInfo = $crawler->getBookInfo($kmcode);
 
         // Assert
         $expected = [
@@ -32,6 +31,29 @@ class testKingStoneCrawler extends PHPUnit\Framework\TestCase
             'translator'  => '魏嘉儀',
         ];
 
-        $this->assertEquals($expected, $crawler->getBookInfo());
+        $this->assertEquals($expected, $bookInfo);
+    }
+
+    public function testSearchBookByName()
+    {
+        $client  = new Client();
+        $crawler = new KingStoneCrawler($client);
+
+        $bookName = '不需要電腦的程式設計課：從遊戲中學習電腦語言';
+        $books = $crawler->searchBook($bookName);
+
+        $expected = [
+            [
+                'ISBN'        => '9789864591374',
+                'name'        => '不需要電腦的程式設計課：從遊戲中學習電腦語言、鍛鍊運算思維，培育AI時代必備的數位素養',
+                'subTitle'    => 'How to Think Like a Coder without Even Trying',
+                'author'      => '養姆．克利斯欽',
+                'publishing'  => '積木      ',
+                'publishDate' => '2018/6/7',
+                'translator'  => '魏嘉儀',
+            ],
+        ];
+
+        $this->assertEquals($expected, $books);
     }
 }
